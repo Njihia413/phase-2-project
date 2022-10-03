d755043842dda8bef2a5c8683255b7fc6a856a24
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-03 18:34:54 +0300

    Fetch BBC News articles

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -3,7 +3,9 @@ import ArticlesList from "./ArticlesList";
 
 function Articles () {
     const cnnArticlesUrl = "https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=e05ca232a09f4c6394d6716dbcde7fef&pageSize=8";
+    const bbcArticlesUrl  = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=e05ca232a09f4c6394d6716dbcde7fef&pageSize=8"
     const [cnnArticles, setcnnArticles] = useState([]);
+    const [bbcArticles, setbbcArticles] = useState([]);
 
     //Fetch CNN Articles
     useEffect(() => {
@@ -11,10 +13,18 @@ function Articles () {
         .then(response => response.json())
         .then(data => setcnnArticles(data.articles))
     },[])
+    
+
+    //Fetch BBC Articles
+     useEffect(() => {
+        fetch(`${bbcArticlesUrl}`)
+        .then(response => response.json())
+        .then(data => setbbcArticles(data.articles))
+    },[])
 
     return (
         <div>
-            <ArticlesList cnnArticles={cnnArticles} setcnnArticles={setcnnArticles}/>
+            <ArticlesList cnnArticles={cnnArticles} bbcArticles={bbcArticles} setcnnArticles={setcnnArticles} setbbcArticles={setbbcArticles}/>
         </div>
     )
 }
