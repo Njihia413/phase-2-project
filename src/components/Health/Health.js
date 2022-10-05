00af9b92858162ad7ea5fb12207600af014949fc
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-06 00:31:05 +0300

    Fetch and Render Health articles

---
+++ src/components/Health/Health.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import HealthCard from "./HealthCard";
+
+function Health () {
+    const healthUrl = "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=1e0619eab0ac48a8b63e0e1820c18d7a&pageSize=12"
+    const [health, setHealth] = useState([]);
+
+    //Fetch Health Data
+    useEffect(() => {
+        fetch(`${healthUrl}`)
+        .then(response => response.json())
+        .then(data => setHealth(data.articles))
+    })
+
+    const healthList = health.map((healthdata => {
+        return <HealthCard
+        key={healthdata.name}
+        healthdata = {healthdata}
+        />
+    }))
+
+    return (
+        <>
+        <div className="top-news m-2">
+            <h3 className="text-uppercase headings text-center mt-5 mb-4">all news articles <br></br><small>Health News</small></h3>
+            <div className="row">
+                {healthList}
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Health;
